8568d89deff3c11e7386d94f41b3c8cb5ef8c3f2
Amar Sinha <sinhaamar68@gmail.com>
2020-06-22 23:15:04 +0530

    added main repo readme

---
+++ stringRepeat/README.md
@@ -0,0 +1,9 @@
+# Objective :
+    Write a function that takes in a string and number that repeats the string that number of times.
+# Examples :
+* yourFunction('Amar',3) will return AmarAmarAmar
+* yourFunction("Akhi",5) will return AkhiAkhiAKhiAkhiAkhi
+# Key Features Used :
+* concaat();
+* while() loop
+---
---
+++ stringRepeat/stringRepeat.js
@@ -0,0 +1,15 @@
+function stringRepeat(str, num) {
+    let repeatedString = "";
+    while (num > 0) {
+        stringRepeat = stringRepeat.concat(str);
+        num--;
+    }
+    return stringRepeat;
+}
+
+function repeatString(str, num) {
+    return str.repeaat(num);
+}
+
+console.log("Amar", 3);
+console.log("Akhi", 5);
